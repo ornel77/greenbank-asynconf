fb485fcd0cd0d579d169cc7ae85002ffd93f851d
ornel77 <ornella.tomboza@gmail.com>
2023-10-30 14:50:49 +0100

    ui for the form ok

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar class="mb-9 flex">
+			<div class="flex w-full justify-between gap-6">
+
+				<h3 class="text-left">Greenbank</h3>
+				<LightSwitch class=""/>
+			</div>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container mx-auto overflow-hidden lg:max-w-[600px] px-4">
+	<h1 class="text-2xl mb-4">Remplisser ce formulaire pour connaître votre impact écologique</h1>
+	<form action="">
+		<label for="" class="mb-4">
+			Type de voiture
+			<select name="" id="" class="select">
+				<option value="citadin">Citadine</option>
+				<option value="citadin">Citadine</option>
+				<option value="citadin">Citadine</option>
+			</select>
+		</label>
+		<label for="" class="mb-4">
+			Energie
+			<select name="" id="" class="select">
+				<option value="diesel">Diesel</option>
+				<option value="diesel">Diesel</option>
+				<option value="diesel">Diesel</option>
+			</select>
+		</label>
+		<label for="" class="label mb-4">
+			Kilométrage
+			<input type="number" name="" id="" class="input" />
+		</label>
+		<label for="" class="label mb-4">
+			Année
+			<input type="number" name="" id="" class="input" />
+		</label>
+
+		<label for="" class="label mb-4">
+			Nombre de passagers
+			<select name="" id="" class="select">
+				<option value="1">1 Passager</option>
+				<option value="2">2 Passagers</option>
+				<option value="3">3 Passagers</option>
+				<option value="4">4 Passagers</option>
+			</select>
+		</label>
+		<div class="flex justify-center">
+
+			<button type="submit" class="btn btn-sm variant-filled-primary w-56">Envoyer</button>
+		</div>
+	</form>
+</div>
